6fa47dab7975a38702983019a5bf0abb271b5ed0
Das <sibendu.das@gmail.com>
2022-05-28 17:29:17 +0530

    .

--- src/common/leftbar.js
+++ src/common/leftbar.js
@@ -16,78 +16,78 @@ function LeftBar(props) {
     return (
         <div id="layoutDrawer_nav">
                
-                <nav class="drawer accordion drawer-light bg-white" id="drawerAccordion">
-                    <div class="drawer-menu">
-                        <div class="nav">
+                <nav className="drawer accordion drawer-light bg-white" id="drawerAccordion">
+                    <div className="drawer-menu">
+                        <div className="nav">
                            
-                            <div class="drawer-menu-divider d-sm-none"></div>
+                            <div className="drawer-menu-divider d-sm-none"></div>
                            
-                            <div class="drawer-menu-heading">Interface</div>
+                            <div className="drawer-menu-heading">Interface</div>
                            
-                            <a class="nav-link" href="#" onClick={()=>handleClick("Home")}>
-                                <div class="nav-link-icon"><i class="material-icons">language</i></div>
+                            <a className="nav-link" href="#" onClick={()=>handleClick("Home")}>
+                                <div className="nav-link-icon"><i className="material-icons">language</i></div>
                                 Home
                             </a>
                             
-                            <a class="nav-link collapsed" href="javascript:void(0);" data-bs-toggle="collapse" data-bs-target="#collapseDashboards" aria-expanded="false" aria-controls="collapseDashboards">
-                                <div class="nav-link-icon"><i class="material-icons">dashboard</i></div>
+                            <a className="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseDashboards" aria-expanded="false" aria-controls="collapseDashboards">
+                                <div className="nav-link-icon"><i className="material-icons">dashboard</i></div>
                                 Customer (test purpose)
-                                <div class="drawer-collapse-arrow"><i class="material-icons">expand_more</i></div>
+                                <div className="drawer-collapse-arrow"><i className="material-icons">expand_more</i></div>
                             </a>
                             
-                            <div class="collapse" id="collapseDashboards" aria-labelledby="headingOne" data-bs-parent="#drawerAccordion">
-                                <nav class="drawer-menu-nested nav">
-                                    <a href="#" class="nav-link" onClick={()=>handleClick("CustomerAllView")}>Customer</a>
+                            <div className="collapse" id="collapseDashboards" aria-labelledby="headingOne" data-bs-parent="#drawerAccordion">
+                                <nav className="drawer-menu-nested nav">
+                                    <a href="#" className="nav-link" onClick={()=>handleClick("CustomerAllView")}>Customer</a>
                                 </nav>
                             </div>
                            
-                            <a class="nav-link collapsed" href="javascript:void(0);" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                                <div class="nav-link-icon"><i class="material-icons">view_compact</i></div>
+                            <a className="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                                <div className="nav-link-icon"><i className="material-icons">view_compact</i></div>
                                 Dashboard
-                                <div class="drawer-collapse-arrow"><i class="material-icons">expand_more</i></div>
+                                <div className="drawer-collapse-arrow"><i className="material-icons">expand_more</i></div>
                             </a>
                            
-                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#drawerAccordion">
-                                <nav class="drawer-menu-nested nav">
-                                    <a class="nav-link" href="layout-dark.html">My tasks</a>
-                                    <a class="nav-link" href="layout-light.html">Menu 2</a>
-                                    <a class="nav-link" href="layout-static.html">Menu 3</a>
+                            <div className="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#drawerAccordion">
+                                <nav className="drawer-menu-nested nav">
+                                    <a className="nav-link" href="layout-dark.html">My tasks</a>
+                                    <a className="nav-link" href="layout-light.html">Menu 2</a>
+                                    <a className="nav-link" href="layout-static.html">Menu 3</a>
                                 </nav>
                             </div>
                             {
                 ((userroles.includes("admin")||userroles.includes("superadmin")) &&
                             <div>
-                            <div class="drawer-menu-divider"></div>
+                            <div className="drawer-menu-divider"></div>
                             
-                            <div class="drawer-menu-heading">Admin</div>
+                            <div className="drawer-menu-heading">Admin</div>
                             
                             
                            
                             
-                            <a class="nav-link collapsed" href="javascript:void(0);" data-bs-toggle="collapse" data-bs-target="#collapseForms" aria-expanded="false" aria-controls="collapseForms">
-                                <div class="nav-link-icon"><i class="material-icons">description</i></div>
+                            <a className="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseForms" aria-expanded="false" aria-controls="collapseForms">
+                                <div className="nav-link-icon"><i className="material-icons">description</i></div>
                                 BPM
-                                <div class="drawer-collapse-arrow"><i class="material-icons">expand_more</i></div>
+                                <div className="drawer-collapse-arrow"><i className="material-icons">expand_more</i></div>
                             </a>
                            
-                            <div class="collapse" id="collapseForms" aria-labelledby="headingOne" data-bs-parent="#drawerAccordion">
-                                <nav class="drawer-menu-nested nav">
-                                    <a class="nav-link" href="#" onClick={()=>handleClick("ProcessDefintionAllView")}>Process Definition</a>
-                                    <a class="nav-link" href="forms-checks-and-radios.html">Menu 2</a>
+                            <div className="collapse" id="collapseForms" aria-labelledby="headingOne" data-bs-parent="#drawerAccordion">
+                                <nav className="drawer-menu-nested nav">
+                                    <a className="nav-link" href="#" onClick={()=>handleClick("ProcessDefintionAllView")}>Process Definition</a>
+                                    <a className="nav-link" href="forms-checks-and-radios.html">Menu 2</a>
                                    
                                 </nav>
                             </div>
                             
-                            <a class="nav-link collapsed" href="javascript:void(0);" data-bs-toggle="collapse" data-bs-target="#collapseUtilities" aria-expanded="false" aria-controls="collapseUtilities">
-                                <div class="nav-link-icon"><i class="material-icons">build</i></div>
+                            <a className="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseUtilities" aria-expanded="false" aria-controls="collapseUtilities">
+                                <div className="nav-link-icon"><i className="material-icons">build</i></div>
                                 Utilities
-                                <div class="drawer-collapse-arrow"><i class="material-icons">expand_more</i></div>
+                                <div className="drawer-collapse-arrow"><i className="material-icons">expand_more</i></div>
                             </a>
                             
-                            <div class="collapse" id="collapseUtilities" aria-labelledby="headingOne" data-bs-parent="#drawerAccordion">
-                                <nav class="drawer-menu-nested nav">
-                                    <a class="nav-link" href="utilities-background.html">Menu 1</a>
-                                    <a class="nav-link" href="utilities-borders.html">Menu 2</a>
+                            <div className="collapse" id="collapseUtilities" aria-labelledby="headingOne" data-bs-parent="#drawerAccordion">
+                                <nav className="drawer-menu-nested nav">
+                                    <a className="nav-link" href="utilities-background.html">Menu 1</a>
+                                    <a className="nav-link" href="utilities-borders.html">Menu 2</a>
                                  
                                 </nav>
                             </div>
@@ -97,12 +97,12 @@ function LeftBar(props) {
                         </div>
                     </div>
                     
-                    <div class="drawer-footer border-top">
-                        <div class="d-flex align-items-center">
-                            <i class="material-icons text-muted">account_circle</i>
-                            <div class="ms-3">
-                                <div class="caption">Logged in as:</div>
-                                <div class="small fw-500">Start Bootstrap</div>
+                    <div className="drawer-footer border-top">
+                        <div className="d-flex align-items-center">
+                            <i className="material-icons text-muted">account_circle</i>
+                            <div className="ms-3">
+                                <div className="caption">Logged in as:</div>
+                                <div className="small fw-500">Start Bootstrap</div>
                             </div>
                         </div>
                     </div>
